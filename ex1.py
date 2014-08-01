d67a1560a4f220fc052f6cd76c1e4002c28d2c4f
nankyoku <alexandra.lemus.r@gmail.com>
2014-08-01 17:13:15 -0400

    Excercise 1

---
+++ ex1.py
@@ -0,0 +1,20 @@
+# Ok, I wanted to cheat and name this file lpthw01.py, but well..
+# If I was in Mexico, I might have to use the following line (with the # included, that line is not intended to be a comment). 
+# -*- coding: utf-8 -*-
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "this is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# Study drills
+#
+#1. Make your script print another line. 
+print "Here, have your new line!"
+#2. Make your script print only one of the lines.
+print "I'm lazy, I would either need to erase all lines but this one, or just use a '#' to comment them. But I'm too excited because I just found out that '#' is called an octothorpe! It is also called pound, hash or mesh. I pick octothorpe!" 
+# Put a '#' (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+# I already knew that '#' allows you to comment your scripts, this is, you can write notes for your self that won't be interpreted by Python... I will leave it there, but I really want to understand that part about Python being an interpreted language. 

e0c399f37402007d68df08ed07633863d7eb0fb7
nankyoku <alexandra.lemus.r@gmail.com>
2014-08-01 16:58:01 -0400

    second commit?

--- lpthw00.py
+++ lpthw00.py
@@ -3,9 +3,15 @@ print "Hello World!\n"
 # I also know how to run this in the command line!
 # $ python lpthw00.py
 # Woo Hoo!
-
+#
 # Note that I have also "read" this lesson (exercise 0), I mostly skipped the information for MacOS and Windows, and I am using Ubuntu 12.04, where I have Python 2.7.3 installed. 
-
+#
 # I also liked google, so I will memorize the "python list functions" phrase to google it now and then. (Check http://instagram.com/p/q-tX-1JKGz), hopefully it will work. 
-
+#
 # Ok, now onto exercise 1...
+#
+# p.s.
+# I must confess that I love Zed's Warnings for beginners!
+#
+# Update:
+# So I went onto exercise 1, but there was that warning about reading absolutely everything, so forget about me skipping everything, I did end up reading the whole exercise 0. 

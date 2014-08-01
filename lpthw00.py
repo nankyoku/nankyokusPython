ea3cdf6ab1485bf7ad9dfd8627945ac447b33300
nankyoku <alexandra.lemus.r@gmail.com>
2014-08-01 17:12:06 -0400

    I think I'm getting a grasp of this

--- lpthw00.py
+++ lpthw00.py
@@ -1,4 +1,4 @@
-# So this is me being a smartie pants, because I know a tiny little bit of python, enough to write comments in python scripts and do the following:
+# So this is me being a smartie pants, because I know a tiny little bit of python, enough to write comments in Python scripts and do the following:
 print "Hello World!\n"
 # I also know how to run this in the command line!
 # $ python lpthw00.py
@@ -6,7 +6,7 @@ print "Hello World!\n"
 #
 # Note that I have also "read" this lesson (exercise 0), I mostly skipped the information for MacOS and Windows, and I am using Ubuntu 12.04, where I have Python 2.7.3 installed. 
 #
-# I also liked google, so I will memorize the "python list functions" phrase to google it now and then. (Check http://instagram.com/p/q-tX-1JKGz), hopefully it will work. 
+# I also like Google, so I will memorize the "python list functions" phrase to google it now and then. (Check http://instagram.com/p/q-tX-1JKGz, hopefully it will work.)
 #
 # Ok, now onto exercise 1...
 #

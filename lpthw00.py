8ca823a4c15085a265e9d54a518016f4367afafa
nankyoku <alexandra.lemus.r@gmail.com>
2014-08-01 16:45:19 -0400

    I am also learning how to use github, this is my second commit, but I am not sure what I am committing

---
+++ lpthw00.py
@@ -0,0 +1,11 @@
+# So this is me being a smartie pants, because I know a tiny little bit of python, enough to write comments in python scripts and do the following:
+print "Hello World!\n"
+# I also know how to run this in the command line!
+# $ python lpthw00.py
+# Woo Hoo!
+
+# Note that I have also "read" this lesson (exercise 0), I mostly skipped the information for MacOS and Windows, and I am using Ubuntu 12.04, where I have Python 2.7.3 installed. 
+
+# I also liked google, so I will memorize the "python list functions" phrase to google it now and then. (Check http://instagram.com/p/q-tX-1JKGz), hopefully it will work. 
+
+# Ok, now onto exercise 1...

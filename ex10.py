9277b84fcd8616c0648907abf3852c4cbe313448
nankyoku <alexandra.lemus.r@gmail.com>
2014-08-11 15:21:31 -0400

    Exercise 10 again

--- ex10.py
+++ ex10.py
@@ -15,3 +15,25 @@ print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+# ADDENDUM
+
+print "\\\\ = \\ \n"
+print "\\\' = \' \n"
+print "\\\" = \" \n"
+print "bla\\abla = bla\abla \n"
+print "bla\\bbla = bla\bbla \n"
+print "bla\\fbla = bla\fbla \n"
+print "bla\\nbla = bla\nbla \n"
+print "bla\\N{name}bla = bla\N{name}bla \n"
+print "bla\\rbla = bla\rbla \n"
+print "bla\\tbla = bla\tbla \n"
+print "\\u0ABC = \u0ABC \n"
+print "\\U0ABC1111 = \U0ABC1111 \n"
+print "bla\\vbla = bla\vbla \n"
+print "\\777 = \777 \n"
+print "\\x21 = \x21 \n"
+
+while True:
+	for i in ["/", "-", "|", "\\", "|"]:
+		print "%s\r" % i,

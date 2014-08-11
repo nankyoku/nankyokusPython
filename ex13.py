e372b69a61724d5b9faaed11b236aea7d101814b
nankyoku <alexandra.lemus.r@gmail.com>
2014-08-11 15:46:11 -0400

    Exercise 13

---
+++ ex13.py
@@ -0,0 +1,16 @@
+# Exercise 13: Parameters, Unpacking, Variables
+
+from sys import argv
+
+script, first, second, third, fourth, fifth, sixth = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "How many more can this variable hold? Fourth is %r, fifth is %r, and sixth is %r." % (fourth,five,sixth)
+
+# run as follows:
+# python script.py first second third
+
+
